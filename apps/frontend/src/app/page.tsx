6cedaa56d438f3953c2790f11300e442b772a07c
Emmanuel Alabi <bobslegend795@gmail.com>
2025-01-24 11:01:31 +0100

    check

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,20 @@
+import { IFailedResponse } from "@skyned/interfaces";
+
 export default function Home() {
+  const res: IFailedResponse = {
+    statusCode: 400,
+    message: "Error",
+    success: false,
+    data: null,
+  };
+
+  console.log(res);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold header">Hello World</h1>
-        <p>Whereas disregard and contempt for human rights have resulted</p>
+        <h1 className="text-2xl font-bold header">Frontend</h1>
+        <p>{process.env.API_URL}</p>
       </div>
     </div>
   );
