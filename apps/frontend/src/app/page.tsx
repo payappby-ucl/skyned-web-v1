48aa0a967afadcb067028d4dc6684e32f4f4d05d
Emmanuel Alabi <bobslegend795@gmail.com>
2025-01-24 08:38:15 +0100

    Removed button

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@workspace/ui/components/button";
-
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold header">Hello World</h1>
         <p>Whereas disregard and contempt for human rights have resulted</p>
-        <Button size="sm" variant="outline">
-          Button
-        </Button>
       </div>
     </div>
   );
