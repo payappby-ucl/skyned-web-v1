6230b345708f0bb15ac4c53fcf3299d27a6f6e34
Emmanuel Alabi <bobslegend795@gmail.com>
2025-01-24 14:34:14 +0100

    fix

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold header">Frontend</h1>
-        <p>{process.env.API_URL}</p>
+        <p>{process.env.NEXT_PUBLIC_API_URL}</p>
       </div>
     </div>
   );
