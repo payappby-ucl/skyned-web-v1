95de04a8cf9a6368db6e24f1b8ce32a9ba6b7255
Emmanuel Alabi <bobslegend795@gmail.com>
2025-03-25 21:48:48 +0100

    rebuild

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,24 +1,9 @@
-import Test from "@/src/components/test";
-
 export default async function Home() {
   return (
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4 py-10">
         <h1 className="text-brand">Emmanuel</h1>
         <h1>Study Abroad H1</h1>
-        <h1>Your sure route to Study Abroad H1</h1>
-        <h2>Study Abroad h2</h2>
-        <h2>Your sure route to Study Abroad h2</h2>
-        <h3>Study Abroad h3</h3>
-        <h3>Your sure route to Study Abroad h3</h3>
-        <h4>Study Abroad h4</h4>
-        <h4>Your sure route to Study Abroad h4</h4>
-        <h5>Study Abroad h5</h5>
-        <h5>Your sure route to Study Abroad h5</h5>
-        <h6>Study Abroad h6</h6>
-        <h6>Your sure route to Study Abroad h6</h6>
-        <p className="text-brand">This is a paragraph</p>
-        <Test />
       </div>
     </div>
   );
