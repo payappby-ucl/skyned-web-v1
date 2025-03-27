f08c55071048430867799f4293046b1e0a1328c4
Emmanuel Alabi <bobslegend795@gmail.com>
2025-03-27 18:20:03 +0100

    update

--- apps/frontend/src/app/_components/testimonials.tsx
+++ apps/frontend/src/app/_components/testimonials.tsx
@@ -17,6 +17,24 @@ const testimonials = [
     imageUrl:
       "https://cdn.pixabay.com/photo/2023/07/25/10/21/girl-8148749_1280.jpg",
   },
+  {
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud",
+    firstName: "Rosemary J.",
+    lastName: "Castillo",
+    title: "Textile cutting machine operator",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2017/09/21/13/32/girl-2771936_1280.jpg",
+  },
+  {
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud",
+    firstName: "Debbie T.",
+    lastName: "McDonough",
+    title: "Floral designer",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2018/06/04/22/55/books-3454398_1280.jpg",
+  },
 ];
 
 const Testimonials: React.FC = () => {
@@ -34,17 +52,29 @@ const Testimonials: React.FC = () => {
           <CarouselContent>
             {testimonials.map(
               ({ comment, firstName, lastName, imageUrl, title }, i) => (
-                <CarouselItem key={`${firstName}${lastName}_${i}_comment`}>
+                <CarouselItem
+                  key={`${firstName}${lastName}_${i}_comment`}
+                  className="basis-2/3"
+                >
                   <Card className="bg-foreground border-brand-50/20">
                     <CardContent>
-                      <div>
+                      <div className="text-background grid grid-cols-2 gap-4">
                         <Image
                           src={imageUrl}
                           alt={`${firstName}${lastName}'s image'`}
                           width={100}
                           height={80}
-                          className="rounded-md"
+                          className="h-[250px] w-full rounded-md object-cover"
                         />
+                        <div className="flex flex-col justify-between">
+                          <p className="text-lg">{comment}</p>
+                          <div>
+                            <p className="text-sm font-bold">
+                              {firstName} {lastName}
+                            </p>
+                            <p className="text-sm">{title}</p>
+                          </div>
+                        </div>
                       </div>
                     </CardContent>
                   </Card>
