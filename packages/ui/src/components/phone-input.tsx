0929da8cfb1f23f1ae9e4a3956819674c6326e01
Emmanuel Alabi <bobslegend795@gmail.com>
2025-04-11 13:29:02 +0100

    fix(Packages/UI): Bug fix on phone input component

--- packages/ui/src/components/phone-input.tsx
+++ packages/ui/src/components/phone-input.tsx
@@ -9,31 +9,54 @@ function PhoneInput(
   props: React.ComponentProps<"input"> & { defaultCountry?: string },
 ) {
   const { value, defaultCountry } = props;
-  const [countryIsoCode, setCountryIsoCode] = React.useState(
-    defaultCountry || "NG",
-  );
   const countries = React.useMemo(() => Country.getAllCountries(), []);
-  const selectedCountry = React.useMemo(
-    () => Country.getCountryByCode(countryIsoCode),
-    [countryIsoCode],
-  );
+
+  const [selectedCountry, setSelectedCountry] = React.useState<{
+    isoCode: string | undefined;
+    flag: string | undefined;
+    name: string | undefined;
+    phonecode: string | undefined;
+  } | null>(() => {
+    const country =
+      countries.find((c) => c.isoCode === defaultCountry || "NG") ||
+      Country.getCountryByCode("NG");
+
+    return {
+      isoCode: country?.isoCode,
+      flag: country?.flag,
+      name: country?.name,
+      phonecode: country?.phonecode,
+    };
+  });
 
   React.useEffect(() => {
-    const asYouType = new AsYouType();
-    asYouType.input(value as string);
-    const countryCode = asYouType.getCallingCode();
-    if (countryCode) {
-      const countryObject = countries.find((c) => c.phonecode === countryCode);
-      if (countryObject) {
-        setCountryIsoCode(countryObject.isoCode);
+    if (!value) {
+      setSelectedCountry(null);
+    }
+    if (!selectedCountry) {
+      const asYouType = new AsYouType();
+      asYouType.input(value as string);
+      const countryCode = asYouType.getCallingCode();
+      if (countryCode) {
+        const countryObject = countries.find(
+          (c) => c.phonecode === countryCode,
+        );
+        if (countryObject) {
+          setSelectedCountry({
+            isoCode: countryObject.isoCode,
+            flag: countryObject.flag,
+            name: countryObject.name,
+            phonecode: countryObject.phonecode,
+          });
+        }
+      } else {
+        setSelectedCountry(null);
       }
-    } else {
-      setCountryIsoCode("");
     }
   }, [value]);
 
   React.useEffect(() => {
-    if (selectedCountry) {
+    if (selectedCountry?.phonecode) {
       const phoneCode = `+${selectedCountry.phonecode
         .substring(
           0,
@@ -66,13 +89,25 @@ function PhoneInput(
     }
   }, [selectedCountry]);
 
+  const handleCountrySelectChange = React.useCallback((isoCode: string) => {
+    const country = Country.getCountryByCode(isoCode);
+    if (country) {
+      setSelectedCountry({
+        isoCode: country.isoCode,
+        flag: country.flag,
+        name: country.name,
+        phonecode: country.phonecode,
+      });
+    }
+  }, []);
+
   return (
     <div
       className="flex items-center border dark:bg-input/30 bg-transparent shadow-xs transition-[color,box-shadow] rounded-md has-aria-invalid:border-destructive has-aria-invalid:ring-destructive/20 has-aria-invalid:dark:ring-destructive/40
     has-focus-visible:border-ring has-focus-visible:ring-ring/50 has-focus-visible:ring-[3px]
     "
     >
-      <Select onValueChange={setCountryIsoCode}>
+      <Select onValueChange={handleCountrySelectChange}>
         <SelectTrigger className="border-0 rounded-r-none border-r">
           <span className="text-lg">{selectedCountry?.flag}</span>
         </SelectTrigger>
@@ -88,7 +123,10 @@ function PhoneInput(
               .replace("+", "")}`;
 
             return (
-              <SelectItem value={country.isoCode} key={country.isoCode}>
+              <SelectItem
+                key={`${country.isoCode}-${country.name}`}
+                value={country.isoCode}
+              >
                 <p className="font-medium text-sm">
                   {country.name} {country.flag}{" "}
                   <span className="text-muted-foreground">{phoneCode}</span>
