d4d53e2e01f0eb59eedc36fa9ff908ce8fb8c21c
Emmanuel Osademe <hrh.osademe@gmail.com>
2025-07-30 16:01:28 +0100

    feat(Schools): Enhance SchoolCard component with improved layout and address display; update SchoolList for better structure; fix URL in Schools page

--- apps/frontend/src/app/schools/_components/card.tsx
+++ apps/frontend/src/app/schools/_components/card.tsx
@@ -1,56 +1,67 @@
 "use client";
 
-import { brandClientApi } from "@/src/lib/client";
 import { ISchool } from "@workspace/shared";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
-import { ArrowRightIcon } from "lucide-react";
+import { MapPinIcon } from "lucide-react";
 import { Button } from "@workspace/ui/components/button";
+import { Card, CardContent } from "@workspace/ui/components/card";
+import { Badge } from "@workspace/ui/components/badge";
 
 interface Props {
   school: ISchool;
 }
 const SchoolCard: React.FC<Props> = ({ school }) => (
-  <article className="space-y-4 overflow-hidden rounded-md border">
-    <Image
-      src={school.logo.url}
-      width={200}
-      height={200}
-      alt={school.name}
-      className="bg-muted h-48 w-full rounded-2xl object-cover"
-    />
-    <div className="space-y-2">
-      <header className="space-y-2">
-        <time
-          dateTime={brandClientApi.date.formatDate(
-            school.createdAt,
-            "YYYY-MM-DD",
-          )}
-          className="font-regular text-xs"
-        >
-          {brandClientApi.date.formatDate(
-            school.updatedAt || school.createdAt,
-            "DD MMMM YYYY",
-          )}
-        </time>
+  <Card className="shadow-none">
+    <CardContent className="h-full flex flex-col space-y-4">
+      {/* Header Section */}
+      <div className="flex items-center gap-4">
+        <div className="relative h-10 w-10 shrink-0 rounded-full border p-2">
+          <Image
+            src={school.logo.url}
+            alt={school.name}
+            width={40}
+            height={40}
+            priority
+            className="object-contain"
+          />
+        </div>
 
-        <h3 className="!text-xl">{school.name}</h3>
-      </header>
+        <div className="flex-1 space-y-2">
+          <h4>{school.name}</h4>
+          <div className="flex items-center gap-4 text-gray-500">
+            <div className="flex items-center gap-1 text-gray-500">
+              <MapPinIcon className="h-4 w-4 shrink-0 text-blue-900" />
+              <span className="text-xs">{school.address}</span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <p className="text-sm">{school.overview}</p>
+      {/* Information Rows */}
+      <div className="space-y-4">
+        <div className="flex items-center justify-between">
+          <span className="text-sm text-gray-500">Institution Type</span>
+          <Badge variant="outline">
+            {school.ownershipType.toLocaleUpperCase()}
+          </Badge>
+        </div>
 
-      <Button asChild>
+        <p className="line-clamp-3 !text-sm text-slate-800 leading-relaxed tracking-normal">{school.overview}</p>
+      </div>
+
+      {/* Action Button */}
+      <Button asChild variant="brand" className="w-full mt-auto">
         <Link
           href={`/schools/${school.slug}`}
           arial-label={`Link to ${school.name} page`}
         >
-            View School Page
-            <ArrowRightIcon className="h-4 w-4" />
+          View {school.name}
         </Link>
       </Button>
-    </div>
-  </article>
+    </CardContent>
+  </Card>
 );
 
 export default SchoolCard;
--- apps/frontend/src/app/schools/_components/list.tsx
+++ apps/frontend/src/app/schools/_components/list.tsx
@@ -9,24 +9,19 @@ interface Props {
 }
 export function SchoolList({ data, searchParams }: Props) {
   return (
-    <section className="grid grid-cols-1 gap-5 !p-0 md:grid-cols-5">
-      <div className="space-y-10 md:col-span-4">
-        <header>
-          <h2>Latest Schools</h2>
-        </header>
-        <div className="hide-scrollbar grid max-h-screen grid-cols-1 items-start gap-8 overflow-x-hidden overflow-y-scroll md:grid-cols-2 lg:grid-cols-3">
-          {data.data.map((school) => (
-            <SchoolCard school={school} key={school.slug} />
-          ))}
-        </div>
-        <SchoolPagination data={data} searchParams={searchParams} />
-        {!data?.total || !data.data.length ? (
-          <div className="text-muted-foreground flex flex-row items-center justify-center gap-2 rounded-md border py-4">
-            <NotebookText size={15} />
-            <p className="text-sm">No Data</p>
-          </div>
-        ) : null}
+    <section className="space-y-6">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+      {data.data.map((school) => (
+        <SchoolCard school={school} key={school.slug} />
+      ))}
       </div>
+      <SchoolPagination data={data} searchParams={searchParams} />
+      {!data?.total || !data.data.length ? (
+        <div className="text-muted-foreground flex flex-row items-center justify-center gap-2 rounded-md border py-4">
+          <NotebookText size={15} />
+          <p className="text-sm">No Data</p>
+        </div>
+      ) : null}
     </section>
   );
 }
