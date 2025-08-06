9616b5efd0f4d08cda89e55366320d3d760cc893
Emmanuel Alabi <bobslegend795@gmail.com>
2025-08-06 15:18:52 +0100

    fix(Frontend): Updated testimonials

--- apps/frontend/src/app/_components/testimonials.tsx
+++ apps/frontend/src/app/_components/testimonials.tsx
@@ -10,28 +10,25 @@ import React from "react";
 const testimonials = [
   {
     comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud",
-    firstName: "Sameer",
-    lastName: "Rai",
-    title: "Product Head",
+      "After three denials, they stepped in and helped me get approved on the first try. They were cool, calm, and really understanding to work with. I can't recommend them enough!",
+    name: "Godwin",
+    title: "Google Business Review",
     imageUrl:
       "https://cdn.pixabay.com/photo/2023/07/25/10/21/girl-8148749_1280.jpg",
   },
   {
     comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud",
-    firstName: "Rosemary J.",
-    lastName: "Castillo",
-    title: "Textile cutting machine operator",
+      "What stood out the most was their genuine passion for connecting students to the best and affordable schools and customer satisfaction. It was evident that they genuinely cared about making my experience the best. I felt well taken care of throughout the entire journey, knowing that I had a reliable and professional team supporting me.",
+    name: "Charles",
+    title: "Google Business Review",
     imageUrl:
       "https://cdn.pixabay.com/photo/2017/09/21/13/32/girl-2771936_1280.jpg",
   },
   {
     comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud",
-    firstName: "Debbie T.",
-    lastName: "McDonough",
-    title: "Floral designer",
+      "They are understanding and most time give you extra guidance on how to present a solid application. I had a little issue at some point, their team scheduled a series of meetings to help me out. Made me feel so important. 😁😁. I highly recommend them for Europe and North American universities and colleges.",
+    name: "Oluwatimilehin ",
+    title: "Google Business Review",
     imageUrl:
       "https://cdn.pixabay.com/photo/2018/06/04/22/55/books-3454398_1280.jpg",
   },
@@ -39,48 +36,44 @@ const testimonials = [
 
 const Testimonials: React.FC = () => {
   return (
-    <section className="bg-[url(/assets/images/backgrounds/students-1.png)] text-background space-y-5">
+    <section className="text-background space-y-5 bg-[url(/assets/images/backgrounds/students-1.png)]">
       <div className="space-y-2 text-center md:flex md:items-center md:justify-between md:space-y-0 md:text-left">
         <h2 className="md:flex-1">Our clients testimonials</h2>
         <p className="md:flex-1">
-          Lorem ipsum dolor sit amet consectetur. Ornare lacus quis tempus massa
-          dignissim quisque. Sagittis fusce vestibu.
+          Discover why students trust us to help them on their study abroad
+          journey.
         </p>
       </div>
       <div>
         <Carousel>
           <CarouselContent>
-            {testimonials.map(
-              ({ comment, firstName, lastName, imageUrl, title }, i) => (
-                <CarouselItem
-                  key={`${firstName}${lastName}_${i}_comment`}
-                  className="md:basis-2/3"
-                >
-                  <Card className="bg-foreground border-brand-50/20">
-                    <CardContent>
-                      <div className="text-background grid grid-cols-1 gap-4 md:grid-cols-2">
-                        <Image
-                          src={imageUrl}
-                          alt={`${firstName}${lastName}'s image'`}
-                          width={100}
-                          height={80}
-                          className="h-[250px] w-full rounded-md object-cover"
-                        />
-                        <div className="flex flex-col justify-between space-y-4 md:space-y-0">
-                          <p className="text-lg">{comment}</p>
-                          <div>
-                            <p className="text-sm font-bold">
-                              {firstName} {lastName}
-                            </p>
-                            <p className="text-sm">{title}</p>
-                          </div>
+            {testimonials.map(({ comment, name, imageUrl, title }, i) => (
+              <CarouselItem
+                key={`${name}_${i}_comment`}
+                className="md:basis-2/3"
+              >
+                <Card className="bg-foreground border-brand-50/20">
+                  <CardContent>
+                    <div className="text-background grid grid-cols-1 gap-4 md:grid-cols-2">
+                      <Image
+                        src={imageUrl}
+                        alt={`${name}'s image'`}
+                        width={100}
+                        height={80}
+                        className="h-[250px] w-full rounded-md object-cover"
+                      />
+                      <div className="flex flex-col justify-between space-y-4 md:space-y-0">
+                        <p className="text-lg">{comment}</p>
+                        <div>
+                          <p className="text-sm font-bold">{name}</p>
+                          <p className="text-sm">{title}</p>
                         </div>
                       </div>
-                    </CardContent>
-                  </Card>
-                </CarouselItem>
-              ),
-            )}
+                    </div>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
           </CarouselContent>
         </Carousel>
       </div>
