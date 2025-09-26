aebf2c0ad0b6487fb01e7337c2b1c7a5ef3d6a36
Emmanuel Alabi <bobslegend795@gmail.com>
2025-09-26 13:16:08 +0100

    doc: Integration test for scholarship

--- apps/backend/functions/__tests__/scholarship.ts
+++ apps/backend/functions/__tests__/scholarship.ts
@@ -4,6 +4,7 @@ import { app } from "../src/app";
 import { scholarshipData } from "../src/data";
 import { StatusCodes } from "http-status-codes";
 import { signInUser } from "./helpers/utils";
+import { responseBody } from "./helpers/constants";
 
 describe("Scholarship API", () => {
   const server = app.getApp();
@@ -78,6 +79,49 @@ describe("Scholarship API", () => {
           });
 
         expect(res.status).toBe(StatusCodes.CONFLICT);
+      });
+    });
+
+    describe(`GET Scholarships - ${url}`, () => {
+      test("should return scholarships list when unauthenticated", async () => {
+        const res = await request(server).get(`${url}`);
+
+        expect(res.status).toBe(StatusCodes.OK);
+        expect(res.body).not.toBeNull();
+        expect(res.body).toEqual({
+          ...responseBody,
+          success: true,
+          data: expect.objectContaining({
+            total: expect.any(Number),
+            perPage: 100,
+            currentPage: 1,
+            nextPage: 2,
+            prevPage: 1,
+          }),
+        });
+      });
+
+      test("should return program list when admin", async () => {
+        const { user } = await signInUser();
+        const token = await user.getIdToken();
+
+        const res = await request(server)
+          .get(`${url}`)
+          .set("authorization", `bearer ${token}`);
+
+        expect(res.status).toBe(StatusCodes.OK);
+        expect(res.body).not.toBeNull();
+        expect(res.body).toEqual({
+          ...responseBody,
+          success: true,
+          data: expect.objectContaining({
+            total: expect.any(Number),
+            perPage: 100,
+            currentPage: 1,
+            nextPage: 2,
+            prevPage: 1,
+          }),
+        });
       });
     });
   });
